430a530271f926e8952f2b9587e67b3bf2e39d0d
kosuke_ohata <kosuke.ohata2@gmail.com>
2016-02-01 18:09:40 +0900

    add animate.css

--- js/scroll.js
+++ js/scroll.js
@@ -3,3 +3,8 @@ $(document).ready(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 	});
 });
+
+$('#collapse_icon').click(function () {
+	alert('test toggle');
+  /*$('#demo2').collapse('toggle');*/
+});
